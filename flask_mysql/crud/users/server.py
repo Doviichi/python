84f07bd0f9b491c7e75d3f9a3409b98f03bdbb63
Dovi Ichilevici <dovi1499@gmail.com>
2021-12-01 13:09:37 -0500

    user CRUD flask

--- flask_mysql/crud/users/server.py
+++ flask_mysql/crud/users/server.py
@@ -3,16 +3,19 @@ app = Flask(__name__)
 
 from users import Users
 
+
+# home page
 @app.route('/')
 def index():
     users = Users.read_all()
     print(users)
     return render_template('/index.html', users = users)
 
-@app.route('/create')
+# create new user
+@app.route('/user/create')
 def show():
     return render_template('create.html')
-
+# create and procces the new user, redirect to home
 @app.route('/add_user', methods=['post'])
 def add_user():
     data = {
@@ -23,6 +26,41 @@ def add_user():
     Users.add_user(data)
     return redirect('/')
 
+# edit user
+@app.route('/user/edit/<int:id>')
+def edit(id):
+    data = {
+        "id":id
+    }
+    return render_template('edit.html', user=Users.get_one(data))
+# process and update user, redirect home
+@app.route('/user/update/<int:id>', methods=['post'])
+def update(id):
+    data = {
+        'id':id,
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email']
+    }
+    Users.update(data)
+    return redirect('/')
+
+# shows individual user
+@app.route('/user/show/<int:id>')
+def show_user(id):
+    data = {
+        'id':id
+    }
+    return render_template('show.html', user=Users.get_one(data))
+
+# delete user *with out warning!* and redirects home
+@app.route('/user/delete/<int:id>')
+def delete(id):
+    data = {
+        'id':id
+    }
+    Users.delete(data)
+    return redirect('/')
 
 
 if __name__=="__main__":
--- flask_mysql/crud/users/users.py
+++ flask_mysql/crud/users/users.py
@@ -1,4 +1,5 @@
 from mysqlconnection import MySQLConnection, connectToMySQL
+from datetime import datetime
 
 class Users:
     def __init__(self, data):
@@ -14,8 +15,7 @@ class Users:
         query = "SELECT * FROM users"
         results = connectToMySQL('new_users_schema').query_db(query)
         users = []
-        if not results:
-            return users
+        
         for user in results:
             users.append( cls(user) )
         return users
@@ -23,4 +23,26 @@ class Users:
     @classmethod
     def add_user(cls, data):
         query = "INSERT INTO users (first_name, last_name, email) VALUES (%(first_name)s, %(last_name)s, %(email)s)"
-        return connectToMySQL('new_users_schema').query_db(query, data)
+        results = connectToMySQL('new_users_schema').query_db(query, data)
+        return results
+
+
+    @classmethod
+    def get_one(cls, data):
+        query = "SELECT * FROM users WHERE id = %(id)s"
+        results = connectToMySQL('new_users_schema').query_db(query, data)
+        return (cls(results[0]))
+
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s, updated_at =NOW() WHERE id = %(id)s"
+        results = connectToMySQL('new_users_schema').query_db(query, data)
+        
+        return results
+
+    @classmethod
+    def delete(cls, data):
+        query = "DELETE FROM users WHERE id = %(id)s"
+        results = connectToMySQL('new_users_schema').query_db(query, data)
+        return results
+
